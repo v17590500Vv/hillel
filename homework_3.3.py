540da325fcbf33b6bccc8b6e999c952c8cac17d7
ihorzharikov <egor.zhar666@gmail.com>
2024-08-13 14:07:45 +0200

    split the list in two

--- homework_3.3.py
+++ homework_3.3.py
@@ -1,5 +1,5 @@
 box = [1, 21, 31, 41, 51, 61]
-middle_index = len(box) // 2
+middle_index = (len(box) + 1) // 2
 first_half = box[:middle_index]
 second_half = box[middle_index:]
 new_box = [first_half] + [second_half]
